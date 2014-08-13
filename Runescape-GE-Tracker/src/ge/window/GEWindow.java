d1e2d7f935431ffb4f29f07e16d1909e77e1854a
RobertMaloney <robertjamesmaloney@gmail.com>
2014-08-13 16:19:27 -0700

    Demo Window

--- Runescape-GE-Tracker/src/ge/window/GEWindow.java
+++ Runescape-GE-Tracker/src/ge/window/GEWindow.java
@@ -1,10 +1,45 @@
 package ge.window;
 
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
 public class GEWindow {
 
+	private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("GE Tracker");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+ 
+        //Create the menu bar.  Make it have a green background.
+        JMenuBar greenMenuBar = new JMenuBar();
+        greenMenuBar.setOpaque(true);
+        greenMenuBar.setBackground(new Color(154, 165, 127));
+        greenMenuBar.setPreferredSize(new Dimension(400, 20));
+ 
+        //Create a yellow label to put in the content pane.
+        JLabel yellowLabel = new JLabel();
+        yellowLabel.setOpaque(true);
+        yellowLabel.setBackground(new Color(248, 213, 131));
+        yellowLabel.setPreferredSize(new Dimension(200, 180));
+ 
+        //Set the menu bar and add the label to the content pane.
+        frame.setJMenuBar(greenMenuBar);
+        frame.getContentPane().add(yellowLabel, BorderLayout.CENTER);
+ 
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Git Test");
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				createAndShowGUI();
+			}
+		});
 	}
 
 }
