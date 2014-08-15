afbd75213cdad7d4e0fa8d804d488331125284aa
RobertMaloney <robertjamesmaloney@gmail.com>
2014-08-15 14:42:56 -0700

    Refactored JsonObject and toString

--- Runescape-GE-Tracker/src/ge/updater/JsonObject.java
+++ Runescape-GE-Tracker/src/ge/updater/JsonObject.java
@@ -1,38 +1,36 @@
 package ge.updater;
 
-import java.util.HashMap;
 import java.util.ArrayList;
 import java.util.Iterator;
 
-public class JsonObject {
+public class JsonObject extends java.util.HashMap<String, Object> {
 	
-	HashMap<String,Object> properties;
-	
-	public JsonObject () {
-		properties = new HashMap<String,Object>();
-	}
+	public JsonObject () {}
 	
 	public JsonObject(String syntax) {
-		properties = parseSyntax(syntax.substring(1,syntax.length()-1));
+		parseSyntax(syntax.substring(1,syntax.length()-1));
 	}
 	
-	private HashMap<String,Object> parseSyntax(String syntax) {
+	private void parseSyntax(String syntax) {
+		final boolean debug = false;
 		final int MODE_STRING = 0, MODE_INT = 1, MODE_ARR = 2, MODE_SKIP = -1;
-		HashMap<String,Object> shell = new HashMap<String,Object>();
+		
 		ArrayList<Object> list = null;
 		boolean idmode = true, ignorecommas = false;
-		int mode = MODE_STRING;
+		int mode = MODE_INT;
 		String idbuf = "";
 		Object valbuf = null;
+		
 		for (int i = 0; i < syntax.length(); ++i) {
-			System.out.println(""+i+": "+syntax.charAt(i));
+			if (debug) System.out.println(""+i+": "+syntax.charAt(i));
 			switch (syntax.charAt(i)) {
 			case ':':
 				idmode = false;
+				mode = MODE_INT;
 				break;
 			case '}':
 			case ',':
-				//idbuf = idbuf.substring(idbuf.indexOf("\"")+1,idbuf.lastIndexOf("\""));
+				if (debug) System.out.println("Adding " + idbuf + ": " + valbuf + ", mode " + mode);
 				if (ignorecommas) {
 					if (valbuf == null)
 						valbuf = "" + syntax.charAt(i);
@@ -41,16 +39,16 @@ public class JsonObject {
 				} else {
 					//Need to strip quotes off of value strings
 					if (mode == MODE_STRING) {
-						shell.put(idbuf,valbuf);
+						this.put(idbuf,valbuf);
 						idbuf = "";
 					} else if (mode == MODE_INT) {
-						shell.put(idbuf,valbuf);
+						this.put(idbuf,Integer.parseInt((String)valbuf));
 						idbuf = "";
 					} else if (mode == MODE_ARR) {
-						System.out.println("Adding element to array.");
+						if (debug) System.out.println("Adding element to array.");
 						list.add(valbuf);
 					} else {
-						mode = MODE_STRING;
+						mode = MODE_INT;
 					}
 					valbuf = null;
 					idmode = true;
@@ -59,17 +57,18 @@ public class JsonObject {
 			case '[':
 				mode = MODE_ARR;
 				list = new ArrayList<Object>();
-				System.out.println("Starting array " + idbuf);
+				if (debug) System.out.println("Starting array " + idbuf);
 				break;
 			case ']':
 				mode = MODE_SKIP;
-				System.out.println("Adding array " + idbuf);
+				if (debug) System.out.println("Adding array " + idbuf);
 				list.add(valbuf);
-				shell.put(idbuf, list);
+				this.put(idbuf, list);
 				break;
 			case '{':
-				String newsyntax = syntax.substring(i+1);
-				int newi = 0, brackcount = 1;
+				if (mode != MODE_ARR) mode = MODE_STRING;
+				String newsyntax = syntax.substring(i);
+				int newi = 0, brackcount = 0;
 				for (;newi < newsyntax.length(); ++newi) {
 					if (newsyntax.charAt(newi) == '{')
 						brackcount++;
@@ -79,14 +78,16 @@ public class JsonObject {
 					if (brackcount == 0)
 						break;
 				}
-				i = Math.min(i + newi + 1, syntax.length());
-				System.out.println("Recursing...");
-				valbuf = parseSyntax(newsyntax.substring(0,newi+1));
-				System.out.println("Out of recursion.");
+				i = Math.min(i + newi, syntax.length());
+				if (debug) System.out.println("Recursing...");
+				valbuf = new JsonObject(newsyntax.substring(0,newi+2));
+				if (debug) System.out.println("Out of recursion.");
 				break;
 			case '\"':
-				if (!idmode)
+				if (!idmode) {
 					ignorecommas = !ignorecommas;
+					mode = MODE_STRING;
+				}
 				break;
 			case '\n':
 				break;
@@ -109,16 +110,35 @@ public class JsonObject {
 				break;
 			}
 		}
-		return shell;
 	}
 	
+	//change to output syntactically correct json
 	public String toString() {
-		String res = "";
-		Iterator<String> keys = properties.keySet().iterator();
+		String res = "{";
+		Iterator<String> keys = this.keySet().iterator();
 		while(keys.hasNext()) {
 			String key = keys.next();
-			res += key + "\n";
+			res += "\"" + key + "\":";
+			Object value = this.get(key);
+			if (value instanceof String)
+				res += "\"" + value + "\"";
+			else if (value instanceof Integer)
+				res += value;
+			else if (value instanceof ArrayList<?>) {
+				ArrayList<JsonObject> list = (ArrayList<JsonObject>) value;
+				res += "[";
+				for (int i = 0; i < list.size(); ++i) {
+					res += list.get(i).toString();
+					if (i != list.size() - 1)
+						res += ",";
+				}
+				res += "]";
+			} else if (value instanceof JsonObject)
+				res += ((JsonObject) value).toString();
+			
+			if (keys.hasNext()) res += ",";
 		}
+		res += "}";
 		return res;
 	}
 }
