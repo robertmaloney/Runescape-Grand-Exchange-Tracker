176b11a6f167a63228caf9c50783317d1bcb06ad
RobertMaloney <robertjamesmaloney@gmail.com>
2014-08-14 23:01:07 -0700

    More parse development.

--- Runescape-GE-Tracker/src/ge/updater/JsonObject.java
+++ Runescape-GE-Tracker/src/ge/updater/JsonObject.java
@@ -1,6 +1,8 @@
 package ge.updater;
 
 import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class JsonObject {
 	
@@ -15,54 +17,78 @@ public class JsonObject {
 	}
 	
 	private HashMap<String,Object> parseSyntax(String syntax) {
+		final int MODE_STRING = 0, MODE_INT = 1, MODE_ARR = 2, MODE_SKIP = -1;
 		HashMap<String,Object> shell = new HashMap<String,Object>();
+		ArrayList<Object> list = null;
 		boolean idmode = true;
+		int mode = MODE_STRING;
 		String idbuf = "";
 		Object valbuf = null;
 		for (int i = 0; i < syntax.length(); ++i) {
+			System.out.println(""+i+": "+syntax.charAt(i));
 			switch (syntax.charAt(i)) {
 			case ':':
 				idmode = false;
 				break;
 			case ',':
-				idbuf = idbuf.substring(idbuf.indexOf("\"")+1,idbuf.lastIndexOf("\""));
+				//idbuf = idbuf.substring(idbuf.indexOf("\"")+1,idbuf.lastIndexOf("\""));
 				//Need to strip quotes off of value strings
-				shell.put(idbuf,valbuf);
+				if (mode == MODE_STRING)
+					shell.put(idbuf,valbuf);
+				else if (mode == MODE_INT) {
+					shell.put(idbuf,valbuf);
+				} else if (mode == MODE_ARR) {
+					list.add(valbuf);
+				}
 				idbuf = "";
 				valbuf = null;
 				idmode = true;
+				mode = MODE_STRING;
 				break;
 			case '[':
-				String newsyntax = syntax.substring(i+1);
-				int newi = i+1, brackcount = 1;
-				for (;newi < syntax.length(); ++newi) {
-					if (syntax.charAt(newi) == '[')
-						brackcount++;
-					else if (syntax.charAt(newi) == ']')
-						brackcount--;
-					
-					if (brackcount == 0)
-						break;
-				}
-				i = newi + 1;
-				valbuf = parseSyntax(newsyntax.substring(0,newi));
+				mode = MODE_ARR;
+				list = new ArrayList<Object>();
+				break;
+			case ']':
+				mode = MODE_SKIP;
+				shell.put(idbuf, list);
 				break;
 			case '{':
 				String newsyntax = syntax.substring(i+1);
-				int newi = i+1, brackcount = 1;
-				for (;newi < syntax.length(); ++newi) {
-					if (syntax.charAt(newi) == '{')
+				int newi = 0, brackcount = 1;
+				for (;newi < newsyntax.length(); ++newi) {
+					if (newsyntax.charAt(newi) == '{')
 						brackcount++;
-					else if (syntax.charAt(newi) == '}')
+					else if (newsyntax.charAt(newi) == '}')
 						brackcount--;
 					
 					if (brackcount == 0)
 						break;
 				}
-				i = newi + 1;
+				i = Math.min(i + newi + 1, syntax.length());
 				valbuf = parseSyntax(newsyntax.substring(0,newi));
+				break;
+			case ' ':
+			case '\n':
+				break;
+			default:
+				if (idmode)
+					idbuf += syntax.charAt(i);
+				else
+					valbuf = (String) valbuf + syntax.charAt(i);
 				break;
 			}
 		}
+		return shell;
+	}
+	
+	public String toString() {
+		String res = "";
+		Iterator<String> keys = properties.keySet().iterator();
+		while(keys.hasNext()) {
+			String key = keys.next();
+			res += key + "\n";
+		}
+		return res;
 	}
 }
