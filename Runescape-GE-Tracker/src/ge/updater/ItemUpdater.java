2d976d9dfef7a3d8517dbbca91353147ebb82377
RobertMaloney <robertjamesmaloney@gmail.com>
2014-08-16 04:56:41 -0700

    Complete JsonObject and Began ItemUpdate

--- Runescape-GE-Tracker/src/ge/updater/ItemUpdater.java
+++ Runescape-GE-Tracker/src/ge/updater/ItemUpdater.java
@@ -2,24 +2,87 @@ package ge.updater;
 
 import java.util.HashMap;
 import java.util.ArrayList;
+import java.net.*;
+import java.io.*;
 
 public class ItemUpdater {
 	
-	HashMap<String,Integer> items;
+	private HashMap<String,Integer> items;
 	
-	public static void update() {
+	public ItemUpdater() {
+		items = new HashMap<String,Integer>();
+	}
+	
+	public void update()  throws Exception {
+		/*
 		JsonObject page = new JsonObject("{\"total\":82,\"items\":[{\"icon\":\"http://services.runescape.com/m=itemdb_rs/4551_obj_sprite.gif?id=4798\",\"icon_large\":\"http://services.runescape.com/m=itemdb_rs/4551_obj_big.gif?id=4798\",\"id\":4798,\"type\":\"Ammo\",\"typeIcon\":\"http://www.runescape.com/img/categories/Ammo\",\"name\":\"Adamant brutal\",\"description\":\"Blunt adamantite arrow...ouch\",\"current\":{\"trend\":\"neutral\",\"price\":238},\"today\":{\"trend\":\"neutral\",\"price\":0},\"members\":\"true\"},{\"icon\":\"http://services.runescape.com/m=itemdb_rs/4551_obj_sprite.gif?id=810\",\"icon_large\":\"http://services.runescape.com/m=itemdb_rs/4551_obj_big.gif?id=810\",\"id\":810,\"type\":\"Ammo\",\"typeIcon\":\"http://www.runescape.com/img/categories/Ammo\",\"name\":\"Adamant dart\",\"description\":\"A deadly throwing dart with an adamant tip.\",\"current\":{\"trend\":\"neutral\",\"price\":14},\"today\":{\"trend\":\"neutral\",\"price\":0},\"members\":\"true\"},{\"icon\":\"http://services.runescape.com/m=itemdb_rs/4551_obj_sprite.gif?id=829\",\"icon_large\":\"http://services.runescape.com/m=itemdb_rs/4551_obj_big.gif?id=829\",\"id\":829,\"type\":\"Ammo\",\"typeIcon\":\"http://www.runescape.com/img/categories/Ammo\",\"name\":\"Adamant javelin\",\"description\":\"An adamant tipped javelin.\",\"current\":{\"trend\":\"neutral\",\"price\":59},\"today\":{\"trend\":\"neutral\",\"price\":0},\"members\":\"false\"},{\"icon\":\"http://services.runescape.com/m=itemdb_rs/4551_obj_sprite.gif?id=867\",\"icon_large\":\"http://services.runescape.com/m=itemdb_rs/4551_obj_big.gif?id=867\",\"id\":867,\"type\":\"Ammo\",\"typeIcon\":\"http://www.runescape.com/img/categories/Ammo\",\"name\":\"Adamant knife\",\"description\":\"A finely balanced throwing knife.\",\"current\":{\"trend\":\"neutral\",\"price\":38},\"today\":{\"trend\":\"neutral\",\"price\":0},\"members\":\"true\"},{\"icon\":\"http://services.runescape.com/m=itemdb_rs/4551_obj_sprite.gif?id=804\",\"icon_large\":\"http://services.runescape.com/m=itemdb_rs/4551_obj_big.gif?id=804\",\"id\":804,\"type\":\"Ammo\",\"typeIcon\":\"http://www.runescape.com/img/categories/Ammo\",\"name\":\"Adamant throwing axe\",\"description\":\"A finely balanced throwing axe.\",\"current\":{\"trend\":\"neutral\",\"price\":144},\"today\":{\"trend\":\"neutral\",\"price\":0},\"members\":\"true\"},{\"icon\":\"http://services.runescape.com/m=itemdb_rs/4551_obj_sprite.gif?id=31597\",\"icon_large\":\"http://services.runescape.com/m=itemdb_rs/4551_obj_big.gif?id=31597\",\"id\":31597,\"type\":\"Ammo\",\"typeIcon\":\"http://www.runescape.com/img/categories/Ammo\",\"name\":\"Azure skillchompa\",\"description\":\"Can be used to enhance gathering of energy, fish, logs and ore, as a rune-equivalent tool, where applicable.\",\"current\":{\"trend\":\"neutral\",\"price\":377},\"today\":{\"trend\":\"neutral\",\"price\":0},\"members\":\"true\"}]}");
-		System.out.println(page.toString());
-		System.out.println(page.get("total") instanceof Integer);
 		ArrayList<JsonObject> items = (ArrayList<JsonObject>) page.get("items");
 		for (int i = 0; i < items.size(); ++i)
 			System.out.println(items.get(i).get("id") + ": " + items.get(i).get("name"));
 		
 		System.out.println(((JsonObject) items.get(0).get("today")).get("price"));
+		
+		System.out.println("New Json");
+		JsonObject page2 = new JsonObject();
+		page2.put("total", 92);
+		page2.put("ids", new int[]{0, 1, 2, 3, 4});
+		page2.put("names", new String[]{"item1", "item2", "item3", "item4", "item5"});
+		System.out.println(page2);
+		*/
+		for (int i = 0; i < 38; ++i) {
+			int counts[] = grabAlphaCounts(i);
+			for (int j = 0; j < counts.length; ++j) {
+				int page = 1;
+				while (counts[j] > 0) {
+					String alpha = (j == 0) ? "%23" : "" + (char) ('a' + j - 1);
+					URL alphas = new URL("http://services.runescape.com/m=itemdb_rs/api/catalogue/items.json?category=" + i + "&alpha=" + alpha + "&page=" + page);
+					URLConnection conn = alphas.openConnection();
+					BufferedReader in = new BufferedReader( new InputStreamReader( conn.getInputStream() ) );
+					JsonObject json = new JsonObject(in.readLine());
+					//System.out.println("\n" + json.toString());
+					ArrayList<JsonObject> items = (ArrayList<JsonObject>) json.get("items");
+					//System.out.println(items.size());
+					for (int k = 0; k < items.size(); ++k) {
+						//System.out.println(items.get(k).toString());
+						String name = (String) items.get(k).get("name");
+						int id = (int) items.get(k).get("id");
+						this.items.put(name, id);
+					}
+					counts[j] -= items.size();
+					++page;
+					Thread.sleep(1000);
+				}
+			}
+		}
 	}
 	
-	public static void main (String [] args) {
-		update();
+	public String toString() {
+		return items.toString();
+	}
+	
+	public int size() {
+		return items.size();
+	}
+	
+	private int[] grabAlphaCounts(int category) throws Exception {
+		URL alphas = new URL("http://services.runescape.com/m=itemdb_rs/api/catalogue/category.json?category=" + category);
+		URLConnection conn = alphas.openConnection();
+		BufferedReader in = new BufferedReader( new InputStreamReader( conn.getInputStream() ) );
+		String jstring = in.readLine();
+		//System.out.println(jstring+"\n");
+		JsonObject json = new JsonObject(jstring);
+		//System.out.println(json.toString());
+		int counts[] = new int[27];
+		for (int i = 0; i < 26; ++i)
+			counts[i] = (int) ((ArrayList<JsonObject>) json.get("alpha")).get(i).get("items");
+		return counts;
+	}
+	
+	public static void main (String [] args) throws Exception {
+		ItemUpdater iu = new ItemUpdater();
+		iu.update();
+		System.out.println(iu.toString());
+		System.out.println(iu.size());
 	}
 	
 	
--- Runescape-GE-Tracker/src/ge/updater/JsonObject.java
+++ Runescape-GE-Tracker/src/ge/updater/JsonObject.java
@@ -11,62 +11,113 @@ public class JsonObject extends java.util.HashMap<String, Object> {
 		parseSyntax(syntax.substring(1,syntax.length()-1));
 	}
 	
+	/*
+	public Object put(String key, Object value) {
+		if (!(value instanceof String) && !(value instanceof Number)
+			&& !(value instanceof JsonObject) && !(value instanceof ArrayList))
+			return null;
+		else
+			return super.put(key, value);
+	}
+	*/
+	
+	/**
+	 * parseSyntax takes as input a string of syntactically correct Json and fills
+	 * the HashMap with its key-value pairs.
+	 * <p>
+	 * This is achieved by a combination of iteration and recursion. The method
+	 * iterates through the string, copying unimportant characters into the idbuf
+	 * and valbuf, which are buffers for a String and Object, respectively. If a
+	 * special character is found, the following is performed:
+	 * -'}', ',': Write the buffers into the HashMap and clear them
+	 * -':': Start writing to valbuf, and default to MODE_INT
+	 * -'[': Begin filling Json array, set to MODE_ARR
+	 * -']': Write Json array to HashMap and skip next comma
+	 * -'{': Recurse into a new JsonObject
+	 * -'"': Switch to MODE_STRING and turn off/on ignoring commas
+	 * 
+	 * @return void
+	 * @author RobertMaloney
+	 * @param syntax the Json string to be parsed
+	 */
 	private void parseSyntax(String syntax) {
+		// Prints strings if debugging
 		final boolean debug = false;
+		if (debug) System.out.println(syntax);
+		
+		// These modes dictate what is put into the HashMap
 		final int MODE_STRING = 0, MODE_INT = 1, MODE_ARR = 2, MODE_SKIP = -1;
 		
+		// Helper variables
 		ArrayList<Object> list = null;
 		boolean idmode = true, ignorecommas = false;
 		int mode = MODE_INT;
 		String idbuf = "";
 		Object valbuf = null;
 		
+		// Iterate over the string
 		for (int i = 0; i < syntax.length(); ++i) {
 			if (debug) System.out.println(""+i+": "+syntax.charAt(i));
 			switch (syntax.charAt(i)) {
+			// Start writing to valbuf, and default to MODE_INT
 			case ':':
 				idmode = false;
 				mode = MODE_INT;
 				break;
+				
+			// Write the buffers into the HashMap and clear them
 			case '}':
 			case ',':
 				if (debug) System.out.println("Adding " + idbuf + ": " + valbuf + ", mode " + mode);
+				
+				// Add commas to valbuf if they are unimportant
 				if (ignorecommas) {
 					if (valbuf == null)
 						valbuf = "" + syntax.charAt(i);
 					else
 						valbuf = valbuf.toString() + syntax.charAt(i);
 				} else {
-					//Need to strip quotes off of value strings
+					// Strings are saved directly, numbers are parsed
 					if (mode == MODE_STRING) {
 						this.put(idbuf,valbuf);
 						idbuf = "";
 					} else if (mode == MODE_INT) {
 						this.put(idbuf,Integer.parseInt((String)valbuf));
 						idbuf = "";
+					// We save the array separately and use this to fill it
+					// idbuf isn't reset because we need it at the end of the array
 					} else if (mode == MODE_ARR) {
 						if (debug) System.out.println("Adding element to array.");
 						list.add(valbuf);
-					} else {
-						mode = MODE_INT;
 					}
+					// Reset valbuf
 					valbuf = null;
 					idmode = true;
 				}
 				break;
+				
+			// Begin filling Json array, set to MODE_ARR
 			case '[':
 				mode = MODE_ARR;
 				list = new ArrayList<Object>();
 				if (debug) System.out.println("Starting array " + idbuf);
 				break;
+				
+			// Write Json array to HashMap and skip next comma
 			case ']':
 				mode = MODE_SKIP;
 				if (debug) System.out.println("Adding array " + idbuf);
 				list.add(valbuf);
 				this.put(idbuf, list);
+				idbuf = "";
 				break;
+				
+			// Recurse into a new JsonObject
 			case '{':
+				// Change to MODE_STRING if valbuf isn't an array
 				if (mode != MODE_ARR) mode = MODE_STRING;
+				
+				// Cut out the Json child object from the mother string
 				String newsyntax = syntax.substring(i);
 				int newi = 0, brackcount = 0;
 				for (;newi < newsyntax.length(); ++newi) {
@@ -78,11 +129,19 @@ public class JsonObject extends java.util.HashMap<String, Object> {
 					if (brackcount == 0)
 						break;
 				}
+				
+				// Place i after the child object
 				i = Math.min(i + newi, syntax.length());
+				
 				if (debug) System.out.println("Recursing...");
+				
+				// Parse child
 				valbuf = new JsonObject(newsyntax.substring(0,newi+2));
+				
 				if (debug) System.out.println("Out of recursion.");
 				break;
+				
+			// Switch to MODE_STRING and turn off/on ignoring commas
 			case '\"':
 				if (!idmode) {
 					ignorecommas = !ignorecommas;
@@ -91,6 +150,8 @@ public class JsonObject extends java.util.HashMap<String, Object> {
 				break;
 			case '\n':
 				break;
+				
+			// We only care about spaces when they are in a String
 			case ' ':
 				if (ignorecommas) {
 					if (valbuf == null)
@@ -99,6 +160,8 @@ public class JsonObject extends java.util.HashMap<String, Object> {
 						valbuf = valbuf.toString() + syntax.charAt(i);
 				}
 				break;
+				
+			// Write any content characters
 			default:
 				if (idmode)
 					idbuf += syntax.charAt(i);
@@ -112,19 +175,34 @@ public class JsonObject extends java.util.HashMap<String, Object> {
 		}
 	}
 	
-	//change to output syntactically correct json
+	/**
+	 * Outputs syntactically correct Json Object code by iterating through
+	 * all keys and ArrayLists and concatenating them to a result String.
+	 * @author RobertMaloney
+	 * @return String
+	 */
 	public String toString() {
 		String res = "{";
 		Iterator<String> keys = this.keySet().iterator();
 		while(keys.hasNext()) {
 			String key = keys.next();
+			//System.out.println(key);
 			res += "\"" + key + "\":";
 			Object value = this.get(key);
 			if (value instanceof String)
 				res += "\"" + value + "\"";
 			else if (value instanceof Integer)
 				res += value;
-			else if (value instanceof ArrayList<?>) {
+			else if (value instanceof Object[]) {
+				Object[] list = (Object[]) value;
+				res += "[";
+				for (int i = 0; i < list.length; ++i) {
+					res += list[i].toString();
+					if (i != list.length - 1)
+						res += ",";
+				}
+				res += "]";
+			} else if (value instanceof ArrayList<?>) {
 				ArrayList<JsonObject> list = (ArrayList<JsonObject>) value;
 				res += "[";
 				for (int i = 0; i < list.size(); ++i) {
